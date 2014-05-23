9f04a8377293ba0b5cb3b02f2edafc0e21dd4498
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-05-23 09:31:05 -0500

    added a button to submit form

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -8,15 +8,19 @@
 	<form method="POST">
     <p>
         <label for="username">Username</label>      
-        <input id="username" name="username" type="text">
+        <input id="username" name="username" type="text" placeholder="username">
     </p>
     <p>
         <label for="password">Password</label>
-        <input id="password" name="password" type="password">
+        <input id="password" name="password" type="password" placeholder="passwords">
     </p>
+    <!-- <p>
+        <input type="submit" name="Login" value="Login">
+    </p> -->
     <p>
-        <input type="submit">
+        <button type="Submit">Login</button>
     </p>
+
 </form>
 
 
