ba1c7fbb4a381e1e72d0f184fde1b8b07bcfa68f
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-06-19 09:49:36 -0500

    added a parent class

--- public/classes/address_data_store.php
+++ public/classes/address_data_store.php
@@ -1,36 +1,6 @@
-<?php
-class AddressDataStore {
+<?php 
+require_once('classes/filestore.php');
 
-    public $filename = '';
-
-    public function __construct($file = 'data/address_book.csv') {
-    	$this->filename = $file;
-    }
-
-    public function read_address_book($array) {
-	    $handle = fopen($this->filename, 'r');
-		while (!feof($handle)){
-	    	$row = fgetcsv($handle);
-	    	if (is_array($row)){
-	        	$array[] = $row;
-	    	} // end of if
-		} //while not end of file
-		return $array;
-    } // end of read_address_book
-
-    public function write_address_book($big_array) 
-    {
-        if(is_writable($this->filename)) {
-	     	$handle = fopen($this->filename, 'w');
-	        foreach($big_array as $value){
-	        	fputcsv($handle, $value);
-	        } // end of foreach
-	    	fclose($handle);
-    	}  //end of if
-    } //end of write_address_book
-
-    // function __destruct() {
-    //     echo 'Class dismissed'. PHP_EOL;
-    // }
-
+class AddressDataStore extends Filestore{
+	//code goes here
 } //end of AddressDataStore
