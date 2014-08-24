1fffea8ba4469e419f1bce2ac46282b457b74bae
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-08-24 00:25:15 -0500

    Reviewing functions in js

--- public/js_fundamentals/helloworld.js
+++ public/js_fundamentals/helloworld.js
@@ -1,4 +1,19 @@
 var $greeting = 'Hello World';
+var sum = 4 + 5,
+	difference = 4 - 5,
+	product = 4 * 5,
+	quotient = 4 / 5;
+// alert($greeting);
+// alert("It's a " + $greeting + " type of day");
 
-alert($greeting);
-alert("It's a " + $greeting + " type of day");
+function doSomething(paramOne, paramTwo) {
+	paramOne = paramOne + 3;
+	paramOne = paramOne + 1;
+	paramOne = paramOne * 8;
+	return paramOne * paramTwo;
+}
+
+var foo = doSomething(3, 2);
+console.log(foo);
+var bar = doSomething(4, 2);
+console.log(bar);
